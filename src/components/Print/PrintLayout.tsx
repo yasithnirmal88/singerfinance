ddea77f5c50b7004f828c0cd15af6ccbbc678025
Yasith Nirmal Peiris <synpeiris@gmail.com>
2026-07-22 18:18:23 +0530

    Bring logo down, reduce title/address 0.5pt, increase header gap

--- src/components/Print/PrintLayout.tsx
+++ src/components/Print/PrintLayout.tsx
@@ -39,7 +39,7 @@ export const PrintLayout: React.FC<PrintLayoutProps> = ({ saleData }) => {
         </div>
 
         <div className="doc-header">
-          <img src="/Singer-Logo.png" alt="Singer Finance" style={{ height: '50px', width: 'auto', objectFit: 'contain', display: 'block', marginBottom: '-6px' }} />
+          <img src="/Singer-Logo.png" alt="Singer Finance" style={{ height: '50px', width: 'auto', objectFit: 'contain', display: 'block', marginBottom: '2px' }} />
           <div className="company-title">Singer Finance (Lanka) PLC</div>
           <div className="company-address">No. 498, R. A. De Mel Mawatha, Colombo 03. Tel : 0112 400 400</div>
         </div>
